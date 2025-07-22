eb106603e18089fb7a28336bb572b2fffbce7527
sungu <cliffland.jasungu@gmail.com>
2025-07-22 16:04:43 +0300

    postcard.tsx

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,10 @@
+const PostCard = () => {
+    return (
+        <div className="p-4 border rounded shadow">
+        <h2 className="text-xl font-semibold">Post Title</h2>
+        <p>Post content goes here...</p>
+        </div>
+    );
+};
+
+export default PostCard;
