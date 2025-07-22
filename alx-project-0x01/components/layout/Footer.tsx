f0a2c4db4131411762a527deae371565a683b051
sungu <cliffland.jasungu@gmail.com>
2025-07-22 16:07:51 +0300

    footer.tsx

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="p-4 bg-gray-200 text-center">
+        <p>&copy; 2025 My App</p>
+        </footer>
+    );
+};
+
+export default Footer;
