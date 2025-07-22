27e0b96cd05cb90a0ae3639f9122f967d1acdb33
sungu <cliffland.jasungu@gmail.com>
2025-07-22 16:07:05 +0300

    header.tsx

---
+++ alx-project-0x01/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="p-4 bg-gray-800 text-white text-center">
+        <h1>My App Header</h1>
+        </header>
+    );
+};
+
+export default Header;
