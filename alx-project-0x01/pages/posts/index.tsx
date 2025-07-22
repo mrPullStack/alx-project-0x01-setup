1f9772501bf7c4d60ff0c92de93df53500e822ee
sungu <cliffland.jasungu@gmail.com>
2025-07-22 17:23:02 +0300

    import header in posts/index

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,8 +1,13 @@
-const Posts = () => {
+import Header from "@/components/layout/Header";
+
+const Posts: React.FC = () => {
     return (
-        <div className="p-4">
-        <h1 className="text-3xl font-bold">Posts Page</h1>
-        </div>
+        <>
+        <Header />
+        <main className="p-8">
+            <h1 className="text-3xl font-bold">Posts Page</h1>
+        </main>
+        </>
     );
 };
 
