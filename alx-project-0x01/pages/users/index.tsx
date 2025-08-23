1d9ffaec2d6e2c98d514a956311238a0a04a3dd2
SuNgu <cliffland.jasungu@gmail.com>
2025-08-23 10:41:40 +0300

    checks for fetching and passing user data to the users page

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -6,17 +6,18 @@ interface UsersPageProps {
     posts: UserProps[];
 }
 
-export default function UsersPage({ posts }: UsersPageProps) {
-    return (
-        <div>
-        <h1>Users</h1>
-        {posts.map((user) => (
-            <UserCard key={user.id} {...user} />
-        ))}
-        </div>
-    );
+function Users({ posts }: UsersPageProps) {
+        return (
+            <div>
+            <h1>Users</h1>
+            {posts.map((user) => (
+                <UserCard key={user.id} {...user} />
+            ))}
+            </div>
+        );
 }
 
+export default Users;
 export async function getStaticProps() {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users: UserProps[] = await res.json();
